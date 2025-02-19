a9f70af7e48d75615eb491f06c70a8c221b05c25
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-02-19 18:59:44 +1100

    Add React basic file structure

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# react-template-repository
-A template repository set up for react app creation
+#rename
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+
+// Global styling
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>
+);
